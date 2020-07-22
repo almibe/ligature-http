90223085fa2979520391f231e2cec2a08226c9ec
Alex Michael Berry <berry3@indiana.edu>
2020-07-22 19:45:34 -0400

    removing old files + moving license back to MPL-2.0

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject . "0.1.0-SNAPSHOT"
-  :description "Library Weasel"
-  :url "https://github.com/almibe/library-weasel"
-  :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns .})
