8ec38d61e67836430d4dbcabb1d5c175e8bb2bfb
berry3 <berry3@indiana.edu>
2020-11-06 23:03:09 -0500

    basic structure for app

--- src/main/scala/dev/ligature/server/LigatureServer.scala
+++ src/main/scala/dev/ligature/server/LigatureServer.scala
@@ -5,9 +5,26 @@
 package dev.ligature.server
 
 import cats.effect.{ExitCode, IO, IOApp}
+import dev.ligature.LigatureInstance
+import dev.ligature.store.mock.LigatureMock
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
 
 object LigatureServer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = IO {
-    println("hello")
-  }.as(ExitCode.Success)
+  override def run(args: List[String]): IO[ExitCode] =
+     LigatureMock.instance.use { store =>
+       IO {
+         setupServer(store)
+       }
+    }.as(ExitCode.Success)
+
+  private def setupServer(ligatureInstance: LigatureInstance): Unit = {
+    ligatureInstance.hashCode()
+    val vertx = Vertx.factory.vertx()
+    val server = vertx.createHttpServer()
+    val router = Router.router(vertx)
+    //TODO setup router
+    server.requestHandler(router).listen(8080)
+    ()
+  }
 }
